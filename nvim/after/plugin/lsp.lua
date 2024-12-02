a3be82013a8dc7fb6f173ed2f0b16d59c2d84bb0
Rick Vergunst <R39969@eon.com>
2024-12-02 17:32:52 +0100

    Add zls and update golang

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -63,8 +63,7 @@ cmp.setup.filetype({"sql"}, {
     }
 })
 
-require("lspconfig").nil_ls.setup({})
-
+require("lspconfig").zls.setup({})
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
@@ -122,9 +121,34 @@ require('mason-lspconfig').setup({
             require("lspconfig").gopls.setup({
                 on_attach = function(client, bufnr)
                     vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
+
+                    vim.api.nvim_create_autocmd('BufWritePre', {
+                        pattern = "*.go";
+                        callback = function ()
+                            local params = vim.lsp.util.make_range_params()
+                            params.context = {only = {"source.organizeImports"}}
+
+                            local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+                            for cid, res in pairs(result or {}) do
+                                for _, r in pairs(res.result or {}) do
+                                    if r.edit then
+                                        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                                        vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                                    end
+                                end
+                            end
+                            vim.lsp.buf.format({async = false})
+                        end
+                    })
                 end,
+                cmd = {"gopls"},
                 settings = {
                     gopls = {
+                        completeUnimported = true,
+                        usePlaceholders = true,
+                        analyses = {
+                            unusedparams = true,
+                        },
                         hints = {
                             assignVariableTypes = true,
                             compositeLiteralFields = true,
